e9fed055b4ecb1a663168a90aadb7a951aa573b3
gabriel_m_silva10 <gabriel_m_silva10@estudante.sesisenai.org.br>
2024-04-19 21:06:08 -0300

    insert metado save convidaods

--- src/main/java/com/convidados/controllers/ConvidadosController.java
+++ src/main/java/com/convidados/controllers/ConvidadosController.java
@@ -5,6 +5,7 @@ import com.convidados.repository.ConvidadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -20,5 +21,14 @@ public class ConvidadosController {
         //envia uma lista "convidados" da tabela convidados
         mv.addObject("convidados", convidadoRepository.findAll());
         return mv;
+    }
+
+    @PostMapping("/convidados")
+    public  String salvar(Convidado convidado){
+        //Chama o metado save do repository e passa o objeto convidado
+        this.convidadoRepository.save(convidado);
+        //atualizar a pagina o front para exibir os dados
+        //redirect faz um get em /convidados
+        return "redirect:/convidados";
     }
 }
